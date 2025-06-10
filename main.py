1bcbecb2b56f5920b827391def338e56bb06e4d1
dragonwarrior61 <amodfired@gmail.com>
2025-06-10 03:34:01 +0300

    configure main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import ssl
+import uvicorn
 import logging
 from pydantic import BaseModel
 
@@ -11,3 +12,38 @@ class MemeberResponse(BaseModel):
     role_name: str
     access_level: str
     
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+ssl_context.load_cert_chain('ssl/cert.pem', keyfile='ssl/key.pem')
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+# async def init_models():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
+        
+# @app.on_event("startup")
+# async def on_startup():
+#     await init_models()
+
+if __name__ == "__main__":
+    
+    ssl_keyfile = "ssl/key.pem"
+    ssl_certfile = "ssl/cert.pem"
+    
+    uvicorn.run(
+        "main:app",
+        host = "0.0.0.0",
+        port = 8000,
+        ssl_keyfile = ssl_keyfile,
+        ssl_certfile = ssl_certfile
+    )
