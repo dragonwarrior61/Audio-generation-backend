8174390e8f255f1ad8753deb42fd4136b9b499ff
dragonwarrior61 <amodfired@gmail.com>
2025-06-10 04:06:44 +0300

    add database.py

---
+++ app/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_sync_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import settings
+
+DATABASE_URL = settings.DATABASE_URL
+
+engine = create_sync_engine(
+    DATABASE_URL,
+    echo=True,
+    future=True,
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=30
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, blind = engine, class_=AsyncSession)
+Base = declarative_base()
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
